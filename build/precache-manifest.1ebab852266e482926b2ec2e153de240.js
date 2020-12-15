811bd17099938df1d55702fe922a99e28c735240
TaeyoungYANG <xodudmaster@naver.com>
2020-12-16 00:00:25 +0900

    first commit

---
+++ build/precache-manifest.1ebab852266e482926b2ec2e153de240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb809e2ae377d1fc42bde7b67684e60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "805c4fc7b99b3fe91146",
+    "url": "/static/js/2.a23328da.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.a23328da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c22ba8089bac50ce11",
+    "url": "/static/js/main.f2a7bea9.chunk.js"
+  },
+  {
+    "revision": "c07fa584632e8cedca0b",
+    "url": "/static/js/runtime-main.b25e5425.js"
+  },
+  {
+    "revision": "28a0058cb357753330c5780350c31f7e",
+    "url": "/static/media/냉장고스몰.28a0058c.png"
+  }
+]);
